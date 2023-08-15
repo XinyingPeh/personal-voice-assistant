0e88d72d15130077b137311356dbdea059297f35
Xinying Peh <jupitoria@gmail.com>
2023-08-15 21:18:29 +0800

    integrate react app into django

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'djangoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'voiceassistant_frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'voiceassistant_frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
