a81a69b2837f4fc6e6ecdc4f5e5bcb3adb0cad2e
Xinying Peh <jupitoria@gmail.com>
2023-08-16 14:39:26 +0800

    add static root

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'voiceassistant_backend',
-    'corsheaders',
-    'rest_framework',
-]
+    'corsheaders',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
