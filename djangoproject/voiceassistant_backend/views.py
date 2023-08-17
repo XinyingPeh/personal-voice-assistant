cea1c60edad6764b0d7385c08064748d41a91985
Xinying Peh <jupitoria@gmail.com>
2023-08-17 16:22:03 +0800

    insert logic for login on home page, include post method, authenticate user

--- djangoproject/voiceassistant_backend/views.py
+++ djangoproject/voiceassistant_backend/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+# Load home page
+def home(request):    
+    # check if logged in
+    if request.method == 'POST':
+        email = request.POST.get('email')   
+        password = request.POST.get('password')
+        # Authenticate user
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials. Please try again.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
