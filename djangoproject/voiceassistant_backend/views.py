1b254aa713222d346271baf005e0f1619873274f
Xinying Peh <jupitoria@gmail.com>
2023-08-17 17:39:54 +0800

    login success with authentication

--- djangoproject/voiceassistant_backend/views.py
+++ djangoproject/voiceassistant_backend/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import get_user_model
 from django.contrib import messages
 
 # Load home page
 def home(request):    
     # check if logged in
     if request.method == 'POST':
-        email = request.POST.get('email')   
+        username = request.POST.get('username')   
         password = request.POST.get('password')
+
         # Authenticate user
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, 'You are now logged in.')
