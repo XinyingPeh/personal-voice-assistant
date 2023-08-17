93b7c18427402c1b45965bd3cf768bccff60067c
Xinying Peh <jupitoria@gmail.com>
2023-08-17 22:27:41 +0800

    create signup form class

---
+++ djangoproject/voiceassistant_backend/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+
+    class Meta:
+        model = User
+        fields = ('username','email', 'password1', 'password2')
+
+
+  
