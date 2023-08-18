3d7c1b08169c378504b29f788031daaa4c6cc5d3
Xinying Peh <jupitoria@gmail.com>
2023-08-18 15:51:03 +0800

    format doc

--- djangoproject/voiceassistant_backend/urls.py
+++ djangoproject/voiceassistant_backend/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), # route to home page
     path('logout', views.logout_user, name='logout'), # route to logout page
-    path('register', views.register_user, name='register') # route to register page
+    path('register', views.register_user, name='register') # route to register page 
 ]
