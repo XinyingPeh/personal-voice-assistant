ce09bb6caabfd93e7568f4d01a39e6200b7a9c8d
Xinying Peh <jupitoria@gmail.com>
2023-08-20 16:31:54 +0800

    add style to list of news

--- djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
+++ djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
@@ -1,11 +1,19 @@
-import NewsCard from "./newscard"
+import NewsCard from "./newscard";
+import { Grow, Grid } from "@mui/material/";
+
 export default function Newscards({ articles }) {
-    return (
-        <div>
-            <h1>List of news</h1>
-            {articles.map((article, i) => (
-                <NewsCard />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h1>List of news</h1>
+      <Grow in>
+        <Grid container alignItems="stretch" spacing={3} padding={5}  margin={0}>
+          {articles.map((article, i) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }} padding={1}>
+              <NewsCard article={article} i={i} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    </div>
+  );
 }
