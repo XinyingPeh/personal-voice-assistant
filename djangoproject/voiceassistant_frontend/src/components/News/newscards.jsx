cd8ecdceb21d6a7873ba0e1cc7efce71960c3ae8
Xinying Peh <jupitoria@gmail.com>
2023-08-21 12:06:31 +0800

    edit newscard style

--- djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
+++ djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
@@ -1,14 +1,13 @@
 import NewsCard from "./newscard";
-import { Grow, Grid } from "@mui/material/";
+import { Grow, Grid, Typography } from "@mui/material/";
 
 export default function Newscards({ articles }) {
   return (
     <div>
-      <h1>List of news</h1>
       <Grow in>
-        <Grid container alignItems="stretch" spacing={3} padding={5}  margin={0}>
+        <Grid container alignItems="stretch" spacing={2} sx={{ margin: '5px', padding: '80px'}}>
           {articles.map((article, i) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }} padding={1}>
+            <Grid item xs={12} sm={4} md={4} lg={3} style={{ display: "flex" }} padding={1}>
               <NewsCard article={article} i={i} />
             </Grid>
           ))}
