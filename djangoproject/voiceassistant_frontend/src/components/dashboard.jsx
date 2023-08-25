a03a00f90debb94e6fbe757ad53be2796925f020
Xinying Peh <jupitoria@gmail.com>
2023-08-25 16:20:31 +0800

    command to open article url by newscard number

--- djangoproject/voiceassistant_frontend/src/components/dashboard.jsx
+++ djangoproject/voiceassistant_frontend/src/components/dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import ApplicationCards from "./applicationcards";
 import Newscards from "./News/newscards";
-
+import wordsToNumbers from "words-to-numbers";
 
 export default function Dashboard() {
     // Set state
@@ -12,12 +12,16 @@ export default function Dashboard() {
     useEffect(() => {
         alanBtn({
             key: process.env.REACT_APP_ALAN_KEY,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if (command === 'newHeadlines') {
                     setNewsArticles(articles);
                     setNewsArticle(-1);
                 } else if (command === 'highlight') {
                     setNewsArticle((prevActiveArticle) => prevActiveArticle + 1); 
+                } else if(command === 'open') {
+                    console.log(number)
+                    console.log(articles[number].url)
+                    window.open(articles[number].url, '_blank');
                 }
             }
         })
