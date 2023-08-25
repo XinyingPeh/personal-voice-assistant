b49a7595e874e25cc69a495a116fb789af05279c
Xinying Peh <jupitoria@gmail.com>
2023-08-25 22:42:27 +0800

    import useref to use alan ai client api, add open article command with validation on article url

--- djangoproject/voiceassistant_frontend/src/components/dashboard.jsx
+++ djangoproject/voiceassistant_frontend/src/components/dashboard.jsx
@@ -1,32 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import ApplicationCards from "./applicationcards";
 import Newscards from "./News/newscards";
 import wordsToNumbers from "words-to-numbers";
 
 export default function Dashboard() {
-    // Set state
-    const [ newsArticles, setNewsArticles ] = useState([]);
-    const [activeArticle, setNewsArticle] = useState(-1);
-    // Alan ai voice command button
-    useEffect(() => {
-        alanBtn({
-            key: process.env.REACT_APP_ALAN_KEY,
-            onCommand: ({ command, articles, number }) => {
-                if (command === 'newHeadlines') {
-                    setNewsArticles(articles);
-                    setNewsArticle(-1);
-                } else if (command === 'highlight') {
-                    setNewsArticle((prevActiveArticle) => prevActiveArticle + 1); 
-                } else if(command === 'open') {
-                    console.log(number)
-                    console.log(articles[number].url)
-                    window.open(articles[number].url, '_blank');
-                }
+  // use Alan ai client API
+  const alanBtnRef = useRef({}).current;
+  // Set state
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setNewsArticle] = useState(-1);
+
+  // Alan ai voice command button
+  useEffect(() => {
+    alanBtnRef.btnInstance = alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setNewsArticle(-1);
+        } else if (command === "highlight") {
+          setNewsArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          console.log(number);
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+
+          if (parsedNumber > articles.length || parsedNumber <= 0) {
+            alanBtnRef.btnInstance.playText("Please provide a valid article number.");
+          } else {
+            const article = articles[parsedNumber - 1];
+
+            if (article && article.url) {
+              window.open(article.url, "_blank");
+              alanBtnRef.btnInstance.playText("Opening...");
+            } else {
+              alanBtnRef.btnInstance.playText("Sorry, the selected article does not have a valid URL.");
             }
-        })
-    }, []);
- 
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
     <>
       <ApplicationCards />
