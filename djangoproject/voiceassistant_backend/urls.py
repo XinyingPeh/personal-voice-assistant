cd3535a37ae1892e33ee41eb46f19a4f5879a687
Xinying Peh <jupitoria@gmail.com>
2023-08-26 23:32:39 +0800

    add dashboard path

--- djangoproject/voiceassistant_backend/urls.py
+++ djangoproject/voiceassistant_backend/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # route to home page
     path('logout', views.logout_user, name='logout'), # route to logout page
     path('register', views.register_user, name='register'), # route to register page 
-    path('dashboard', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
