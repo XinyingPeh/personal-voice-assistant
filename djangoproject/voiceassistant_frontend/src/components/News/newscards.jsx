d0f26608709f72dd2f48c1cc4480e2fe6f463200
Xinying Peh <jupitoria@gmail.com>
2023-08-27 22:18:05 +0800

    edit key prop for newscards to article url instead

--- djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
+++ djangoproject/voiceassistant_frontend/src/components/News/newscards.jsx
@@ -7,7 +7,7 @@ export default function Newscards({ articles, activeArticle }) {
       <Grow in>
         <Grid container alignItems="stretch" spacing={2} sx={{ margin: '5px', padding: '50px'}}>
           {articles.map((article, i) => (
-            <Grid key={i} item xs={12} sm={4} md={4} lg={3} style={{ display: "flex" }} padding={1}>
+            <Grid key={article.url} item xs={12} sm={4} md={4} lg={3} style={{ display: "flex" }} padding={1}>
               <NewsCard article={article} activeArticle={activeArticle} i={i} />
             </Grid>
           ))}
