5545c368d9027c9075fc6768f3eff3a8a3420102
Xinying Peh <jupitoria@gmail.com>
2023-09-01 11:23:24 +0800

    edit sh to update pip version

--- djangoproject/build.sh
+++ djangoproject/build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip to the latest version
+pip install --upgrade pip
+
+# Install project dependencies
 pip install -r requirements.txt
 
+# Other deployment tasks
 python manage.py collectstatic --no-input
 python manage.py migrate
